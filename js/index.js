282c1ec649db8feb32d9f022054544951c752fb5
badrul-islam <bodrulislamkhan10@gmail.com>
2024-09-04 15:50:02 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var myName = "MD Badrul";
+console.log(myName);
+var myName = "Badrul";
+console.log(myName);
+
+let age = "28";
+console.log(age);
+age = "30";
+console.log(age);
+
+const fs = "MD";
+console.log(fs);
+
+function clickMe() {
+    alert("We accept coockies and see your data. " + " " + (30+15))
+}
---
+++ js/main.js
@@ -0,0 +1,136 @@
+(function ($) {
+    "use strict";
+
+    $(window).on('load', function(){
+        //===== Prealoder
+        $("#preloader").delay(400).fadeOut();
+
+        //02. Isotope Initialize
+        function isotopeInit() {
+            $('.project_items').isotope({
+                itemSelector: '.item',
+                masonry: {
+                    columnWidth: '.item'
+                }
+            });
+            $('.project_filter_menu ul li').on('click', function () {
+                $('.project_filter_menu ul li').removeClass('active');
+                $(this).addClass('active');
+                var selector = $(this).attr('data-filter');
+                $('.project_items').isotope({
+                    filter: selector
+                });
+                return false;
+            });
+        }
+        isotopeInit();
+    });
+
+    $(document).ready(function () {
+        //05. sticky header
+        function sticky_header(){
+            var wind = $(window);
+            var sticky = $('header');
+            wind.on('scroll', function () {
+                var scroll = wind.scrollTop();
+                if (scroll < 100) {
+                    sticky.removeClass('sticky');
+                } else {
+                    sticky.addClass('sticky');
+                }
+            });
+        }
+        sticky_header();
+        //===== Back to top
+
+        // Show or hide the sticky footer button
+        $(window).on('scroll', function () {
+            if ($(this).scrollTop() > 600) {
+                $('.back-to-top').fadeIn(200)
+            } else {
+                $('.back-to-top').fadeOut(200)
+            }
+        });
+
+        //Animate the scroll to yop
+        $('.back-to-top').on('click', function (event) {
+            event.preventDefault();
+
+            $('html, body').animate({
+                scrollTop: 0,
+            }, 900);
+        });
+
+        // Hamburger-menu
+        $('.hamburger-menu').on('click', function () {
+            $('.hamburger-menu .line-top, #menu').toggleClass('current');
+            $('.hamburger-menu .line-center').toggleClass('current');
+            $('.hamburger-menu .line-bottom').toggleClass('current');
+        });
+
+
+        //06. magnific Popup Initialize
+        function magnificPopupInit() {
+            $('.content a').magnificPopup({
+                type: 'image',
+                gallery: {
+                    enabled: true
+                }
+            });
+        }
+        magnificPopupInit();
+
+        //10. Client Slider Initialize
+        function client_carouselInit() {
+            $('.owl-carousel.slider1').owlCarousel({
+                loop: true,
+                margin: 30,
+                items: 3,
+                autoplay: true,
+                autoplayTimeout: 1500,
+                autoplayHoverPause: true,
+                nav: true,
+                dots: false,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    390: {
+                        items: 2
+                    },
+                    575:{
+                        items: 3
+                    },
+                    768: {
+                        items: 4
+                    },
+                    992: {
+                        items: 3
+                    }
+                }
+            });
+        }
+        client_carouselInit();
+
+        //11. Video Popup Initialize
+        function videoPopupInit() {
+            $('#play-video').magnificPopup({
+                type: 'iframe',
+                iframe: {
+                    patterns: {
+                        youtube: {
+                          index: 'youtube.com/', 
+
+                          id: 'v=',
+                          src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+                        },
+                    },
+
+                  srcAction: 'iframe_src',
+                }
+            });
+        }
+        videoPopupInit();
+    });
+
+})(jQuery);
